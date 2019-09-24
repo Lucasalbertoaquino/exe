e405d3b2397de7333453f70f373cc2efd75807ee
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2019-09-24 15:32:12 -0300

    Add files via upload

---
+++ exe13.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<string.h>
+
+void ord(int *vec){
+    int i,j,aux;
+        for(i=0;i<9;i++){
+            for(j=i+1;j<10;j++){
+                if(vec[i]>vec[j]){
+                    aux=vec[i];
+                    vec[i]=vec[j];
+                    vec[j]=aux;
+                }
+            }
+        }
+        /*
+        printf("Vetor ordenado!\n");
+        for(i=0;i<10;i++){
+            printf("%d\n",vec[i]);
+        }
+        */
+    }
+void copia(int *vec){
+    int i,j,vecn[30];
+    for(i=0;i<30;i++){
+        vecn[i]=(vec[i]);
+    }//como retornar um vetor
+    printf("\n");
+    ord(&vecn);
+    for(j=0;j<30;j++){
+        printf("%d\n",vecn[j]);
+    }
+}
+
+
+int main(void){
+    int i,j,v1[10]={7,6,5,10,0,40,4,6,9,1},v2[10]={10,7,4,6,30,1,8,45,2},v3[10]={6,4,7,98,3,80,10,43,100,1};
+    int aux,ve[30];
+    int *p;
+
+    for(i=0;i<10;i++){
+        printf("v1[%i]:%d v2[%i]:%d v3[%i]:%d \n",i,v1[i],i,v2[i],i,v3[i]);
+    }
+    ord(&v1);
+    ord(&v2);
+    ord(&v3);
+
+    printf("\n");
+    /*
+    for(i=0;i<10;i++){
+        printf("%d %d %d \n",v1[i],v2[i],v3[i]);
+    }
+    */
+    copia(&v1);
+    copia(&v2);
+    copia(&v3);
+return(0);
+}
